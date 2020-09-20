b9dbb9744a271415169f4dfc0743263449240872
md.ashrafulislam <ksashraful94@gmail.com>
2020-09-20 15:05:42 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,42 @@
+//traverse an array
+let arr=[1,5,66,7,44,90];
+ let sum=0;
+ 
+ for(let i=0; i<arr.length; i++){
+    arr[i]+=2
+}
+
+console.log(arr) 
+
+//sum of array
+
+/* for(let i=0; i<arr.length; i++){
+    sum +=arr[i]
+
+}
+
+ console.log(sum)  */
+
+ //sum of even number 
+
+ /*for(let i=0; i<arr.length; i++){
+    if(arr[i]%2===0){
+        sum=sum+arr[i]
+    }
+        
+        
+    } 
+
+  console.log(sum) */
+
+    //sum of odd number
+
+       /* for(let i=0; i<arr.length; i++){
+        if(arr[i]%2!==0){
+            sum=sum+arr[i]
+        }
+            
+            
+        }
+    
+        console.log(sum)  */
