08e17badd4f7ba73e14b794275a02423d834018a
md.ashrafulislam <ksashraful94@gmail.com>
2020-09-20 16:04:03 +0600

    first commit

---
+++ insertremove.js
@@ -0,0 +1,20 @@
+//insert and remove element
+
+let arr=[3,5,6,2,9,1]
+
+//insert
+
+// arr[3]=8
+// arr.push(8)
+// arr.unshift(8)
+// arr.splice(3,0,8)
+
+//remove
+
+// arr[3]=null/undefine
+// arr.pop()
+// arr.shift()
+// arr.splice(3,1)
+// arr.splice(3,1,8)
+
+console.log(arr)
